3140fc5c6b863779560478c0908274aafa99f6d0
Shaid Azmin <shaidazmin@gmail.com>
2019-05-03 22:20:33 +0600

    use logo on ActionBar

---
+++ app/src/main/java/com/example/nz/uselogoonactionbar/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.nz.uselogoonactionbar;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent("com.example.nz.backbutton.NewActivity");
+                startActivity(intent);
+            }
+        });
+
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setLogo(R.mipmap.ic_launcher);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
+    }
+}
